69d7af2806142bf3cc72ecd1df65fec7250d3555
Niles Rowland <niles.rowland@taylorandfrancis.com>
2017-01-26 07:58:43 -0500

    add tests for model behavior.

--- tests/TestCase/Model/Behavior/CommentableBehaviorTest.php
+++ tests/TestCase/Model/Behavior/CommentableBehaviorTest.php
@@ -2,37 +2,8 @@
 namespace Comments\Test\TestCase\Model\Behavior;
 
 use Cake\TestSuite\TestCase;
-use Comments\Model\Behavior\CommentableBehavior;
 use Cake\ORM\TableRegistry;
-use Cake\ORM\Table;
-use Comments\Model\Entity\Comment;
-use Comments\Model\Table\CommentsTable;
 
-if (!class_exists('Articles')) {
-    class ArticlesTable extends Table
-    {
-        /**
-         * Callback data
-         *
-         * @var array
-         */
-        public $callbackData = [];
-
-        /**
-         * @param array $options
-         */
-        public function initialize(array $options)
-        {
-            $this->addBehavior('Comments.Commentable', [
-                'commentsModel' => 'Comments.Comments',
-                'userModelAlias' => 'UsersTable',
-                'userModel' => 'Users'
-            ]);
-
-            $this->table('articles');
-        }
-    }
-}
 
 /**
  * Comments\Model\Behavior\CommentableBehavior Test Case
@@ -61,9 +32,14 @@ class CommentableBehaviorTest extends TestCase
     public function setUp()
     {
         parent::setUp();
-//        $this->Comments = TableRegistry::get('Comments.Comments');
-        $this->CommentableBehavior = new CommentableBehavior(TableRegistry::get('Comments.Comments'));
-//        debug($this->CommentableBehavior->config('commentsTable'));
+        $this->Comments = TableRegistry::get('Comments.Comments');
+        $this->Comments->addBehavior('Comments.Commentable', [
+            'userModelAlias' => 'Users',
+            'userModelClass' => 'Users.Users',
+            'modelName' => 'Comments.Comments'
+        ]);
+//        debug($this->Comments->find()->all());
+
     }
 
     /**
@@ -73,19 +49,111 @@ class CommentableBehaviorTest extends TestCase
      */
     public function tearDown()
     {
-        unset($this->CommentableBehavior);
-        unset($this->Comments);
         parent::tearDown();
     }
 
     /**
+     *
+     */
+    public function testProcessHam()
+    {
+        $action = 'ham';
+        $items = [
+            '00000000-0000-0000-0000-000000000001' => '1',
+            '00000000-0000-0000-0000-000000000002' => '1',
+            '00000000-0000-0000-0000-000000000003' => '1',
+        ];
+        $results = $this->Comments->process($action, $items);
+        $this->assertEquals('success', $results['type']);
+        $count = 0;
+        foreach ($items as $item => $act) {
+            $comment = $this->Comments->get($item);
+            if ($act) {
+                $count++;
+                $this->assertEquals($comment->is_spam, $action);
+            }
+        }
+        $this->assertEquals($results['count'], $count);
+    }
+
+    /**
+     *
+     */
+    public function testProcessSpam()
+    {
+        $action = 'spam';
+        $items = [
+            '00000000-0000-0000-0000-000000000001' => '1',
+            '00000000-0000-0000-0000-000000000002' => '0',
+            '00000000-0000-0000-0000-000000000003' => '1',
+        ];
+        $results = $this->Comments->process($action, $items);
+        $this->assertEquals('success', $results['type']);
+        $count = 0;
+        foreach ($items as $item => $act) {
+            $comment = $this->Comments->get($item);
+            if ($act) {
+                $count++;
+                $this->assertEquals($comment->is_spam, $action);
+            }
+        }
+        $this->assertEquals($results['count'], $count);
+    }
+
+    /**
+     *
+     */
+    public function testProcessApprove()
+    {
+        $action = 'approve';
+        $items = [
+            '00000000-0000-0000-0000-000000000001' => '1',
+            '00000000-0000-0000-0000-000000000002' => '0',
+            '00000000-0000-0000-0000-000000000003' => '1',
+        ];
+        $results = $this->Comments->process($action, $items);
+
+        $this->assertEquals('success', $results['type']);
+        $count = 0;
+        foreach ($items as $item => $act) {
+            $comment = $this->Comments->get($item);
+            if ($act == 1) {
+                $count++;
+                $this->assertEquals($comment->approved, true);
+            }
+        }
+        $this->assertEquals($results['count'], $count);
+    }
+
+    public function testProcessDisapprove()
+    {
+        $action = 'disapprove';
+        $items = [
+            '00000000-0000-0000-0000-000000000004' => '1',
+            '00000000-0000-0000-0000-000000000005' => '0',
+            '00000000-0000-0000-0000-000000000006' => '1',
+        ];
+        $results = $this->Comments->process($action, $items);
+
+        $this->assertEquals('success', $results['type']);
+        $count = 0;
+        foreach ($items as $item => $act) {
+            $comment = $this->Comments->get($item);
+            if ($act == 1) {
+                $count++;
+                $this->assertEquals($comment->approved, false);
+            }
+        }
+        $this->assertEquals($results['count'], $count);
+    }
+    /**
      * Test commentToggleApprove method
      *
      * @return void
-     */
+     *
     public function testCommentToggleApprove()
     {
-        debug($this->CommentableBehavior->config());
+//        debug($this->CommentableBehavior->config());
 //        $commentId = '00000000-0000-0000-0000-000000000001';
 //        $foreignKey = $this->Comments->get($commentId)->foreignKey;
 //        $assocTable = TableRegistry::get($this->Comments->get($commentId)->model);
